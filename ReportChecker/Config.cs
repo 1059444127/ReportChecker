9cd88b532139332e60cf96b8b24b09e18551c998
Ivan <ivan@izzi.com.ar>
2016-12-01 17:46:19 -0300

    Revert "Revert "First commit. Just the entire project.""
    This reverts commit 2819f021f7d15e0fa5f6db7fa05507fbdc2f2fa3.

---
+++ ReportChecker/Config.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace ReportChecker {
+    public partial class Config : Form {
+        public Config() {
+            InitializeComponent();
+        }
+
+        int fieldsAmount = 6;
+        Label[] label;
+        TextBox[] textBox;
+
+        private void Config_Load(object sender, EventArgs e) {
+            label = new Label[fieldsAmount];
+            textBox = new TextBox[fieldsAmount];
+
+            for (int fieldNumber = 0; fieldNumber < fieldsAmount; ++fieldNumber) {
+                label[fieldNumber] = new Label();
+                textBox[fieldNumber] = new TextBox();
+                textBox[fieldNumber].Size = new Size(200, 20);
+
+                tableLayoutPanel1.Controls.Add(label[fieldNumber]);
+                tableLayoutPanel1.Controls.Add(textBox[fieldNumber]);
+            }
+
+            label[0].Text = "Server address ";
+            textBox[0].Text = Properties.Settings.Default.serverAddress;
+
+            label[1].Text = "Server port ";
+            textBox[1].Text = Properties.Settings.Default.serverPort;
+
+            label[2].Text = "Database ";
+            textBox[2].Text = Properties.Settings.Default.databaseName;
+
+            label[3].Text = "User ";
+            textBox[3].Text = Properties.Settings.Default.databaseUsername;
+
+            label[4].Text = "Password ";
+            textBox[4].UseSystemPasswordChar = true;
+
+            label[5].Text = "Report UNC ";
+            textBox[5].Text = Properties.Settings.Default.reportUNC;
+
+            checkBox1.Checked = Properties.Settings.Default.useWildcard;
+            checkBox2.Checked = Properties.Settings.Default.limitResults;
+            numericUpDown1.Value = Properties.Settings.Default.limitResultsTo;
+
+            textBox[4].Text = SSTCryptographer.Decrypt(
+                                                Properties.Settings.Default.databasePassword,
+                                                Properties.Settings.Default.databaseUsername +
+                                                "as56afs65qer3g654afg");
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            
+            Properties.Settings.Default.serverAddress = textBox[0].Text;
+            Properties.Settings.Default.serverPort = textBox[1].Text;
+            Properties.Settings.Default.databaseName = textBox[2].Text;
+            Properties.Settings.Default.databaseUsername = textBox[3].Text;
+            Properties.Settings.Default.databasePassword =
+                SSTCryptographer.Encrypt(textBox[4].Text, Properties.Settings.Default.databaseUsername + "as56afs65qer3g654afg");
+            Properties.Settings.Default.reportUNC = textBox[5].Text;
+            Properties.Settings.Default.useWildcard = checkBox1.Checked;
+            Properties.Settings.Default.limitResults = checkBox2.Checked;
+            Properties.Settings.Default.limitResultsTo = (int)numericUpDown1.Value;
+            Properties.Settings.Default.Save();
+            Application.Restart();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            dbMySql.dbMySql db = new dbMySql.dbMySql();
+            db.serverAddress = textBox[0].Text;
+            db.serverPort = textBox[1].Text;
+            db.databaseName = textBox[2].Text;
+            db.databaseUsername = textBox[3].Text;
+            db.databasePassword = textBox[4].Text;
+
+            progressBar1.Visible = true;
+            button3.Enabled = false;
+
+            var bgw = new BackgroundWorker();
+            bgw.DoWork += (bgwSender, bgwE) => {
+                ((dbMySql.dbMySql)bgwE.Argument).CreateConnection();
+                ((dbMySql.dbMySql)bgwE.Argument).TestConnection();
+            };
+            bgw.RunWorkerCompleted += dbResult;
+            bgw.RunWorkerAsync(db);
+        }
+
+        private void dbResult(object sender, RunWorkerCompletedEventArgs e) {
+            progressBar1.Visible = false;
+            button3.Enabled = true;
+            if (e.Error == null) {
+                label1.Text = "OK!";
+                label1.ForeColor = System.Drawing.Color.Green;
+            } else {
+                if (e.Error is InvalidCastException) {
+                    label1.Text = "Error de carga";
+                    return;
+                } else if (e.Error is MySqlException) {
+                    label1.ForeColor = System.Drawing.Color.Red;
+                    int errorNumber = ((MySqlException)e.Error).Number;
+                    switch (errorNumber) {
+                        case 1042: // ER_BAD_HOST
+                            label1.Text = "Sin conexión";
+                            break;
+                        case 1043: // ER_HANDSHAKE
+                        case 1044: // ER_DBACCESS_DENIED
+                        case 1045: // ER_ACCESS_DENIED
+                            label1.Text = "Acceso negado";
+                            break;
+                        case 1046: // ER_NO_DB
+                        case 1049: // ER_BAD_DB
+                            label1.Text = "Error nombre db";
+                            break;
+                        default:
+                            label1.Text = "Error N " + errorNumber.ToString();
+                            break;
+                    }
+                } else if (e.Error is ArgumentException || e.Error is InvalidCastException) {
+                    label1.Text = "Error de carga";
+                } else if (e.Error is NullReferenceException) {
+                    label1.Text = "Error programación";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            try {
+                if (Directory.Exists(textBox[5].Text)) {
+                    label3.Text = "OK!";
+                    label3.ForeColor = System.Drawing.Color.Green;
+                } else {
+                    label3.Text = "No existe";
+                    label3.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            catch (Exception) {
+                label3.Text = "Error...";
+                label3.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) {
+            if (numericUpDown1.Value > 1000) {
+                MessageBox.Show("¡Cuidado! Las consultas podrían tardar demasiado tiempo en terminar o,\n" +
+                    "incluso, no completarse nunca.",
+                    "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            if (numericUpDown1.Value < 1) {
+                checkBox2.Checked = false;
+                numericUpDown1.Value = 1;
+            }
+        }
+    }
+}
---
+++ ReportChecker/main.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace ReportChecker
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.flowCommands = new System.Windows.Forms.FlowLayoutPanel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.versionNumber = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblConnectionStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblTryingToRecconect = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(801, 70);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Búsqueda";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(593, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 11;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(347, 16);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(62, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.Text = "Nombre";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(163, 16);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(116, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "N° Acc. (Protocolo)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(10, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.Text = "ID (DNI)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(347, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(240, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Enter += new System.EventHandler(this.textBox3_Enter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(858, 249);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultados";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(852, 230);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.flowCommands);
+            this.groupBox3.Location = new System.Drawing.Point(12, 343);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(811, 49);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            // 
+            // flowCommands
+            // 
+            this.flowCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowCommands.Location = new System.Drawing.Point(3, 16);
+            this.flowCommands.Name = "flowCommands";
+            this.flowCommands.Size = new System.Drawing.Size(805, 30);
+            this.flowCommands.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.versionNumber,
+            this.lblConnectionStatus,
+            this.lblTryingToRecconect,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 395);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(884, 22);
+            this.statusStrip1.TabIndex = 13;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(50, 17);
+            this.toolStripStatusLabel1.Text = "Progreso";
+            this.toolStripStatusLabel1.Visible = false;
+            // 
+            // versionNumber
+            // 
+            this.versionNumber.Name = "versionNumber";
+            this.versionNumber.Size = new System.Drawing.Size(17, 17);
+            this.versionNumber.Text = "v.";
+            // 
+            // lblConnectionStatus
+            // 
+            this.lblConnectionStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblConnectionStatus.Name = "lblConnectionStatus";
+            this.lblConnectionStatus.Size = new System.Drawing.Size(189, 17);
+            this.lblConnectionStatus.Text = "No hay conexión con la base de datos";
+            // 
+            // lblTryingToRecconect
+            // 
+            this.lblTryingToRecconect.Name = "lblTryingToRecconect";
+            this.lblTryingToRecconect.Size = new System.Drawing.Size(129, 17);
+            this.lblTryingToRecconect.Text = "Intentando reconexión...";
+            this.lblTryingToRecconect.Visible = false;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.AutoSize = false;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(200, 16);
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.toolStripProgressBar1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackgroundImage = global::ReportChecker.Properties.Resources.system_config_services;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(829, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 44);
+            this.button2.TabIndex = 14;
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackgroundImage = global::ReportChecker.Properties.Resources.explore_patients_512;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(819, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 64);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Buscar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 417);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(892, 444);
+            this.Name = "main";
+            this.ShowIcon = false;
+            this.Text = "Visualizador de informes previos";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel versionNumber;
+        private System.Windows.Forms.ToolStripStatusLabel lblConnectionStatus;
+        private System.Windows.Forms.ToolStripStatusLabel lblTryingToRecconect;
+        private System.Windows.Forms.FlowLayoutPanel flowCommands;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
